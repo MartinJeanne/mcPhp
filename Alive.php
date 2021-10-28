d876fb4e5f23220a35cf4cd805b904115da38f96
MartinJeanne <martinjeanne.dev@gmail.com>
2021-10-28 16:32:12 +0200

    Beginning of Alive

--- Alive.php
+++ Alive.php
@@ -2,16 +2,31 @@
 class Alive {
   private int $pv;
   private int $pvMax;
+  private int $strength;
 
-  function __construct() {
-    $this->pv = 3;
-    $this->pvMax = 3;
+  function __construct(int $pv = 3, int $strength = 1) {
+    $this->pv = $pv;
+    $this->pvMax = $pv;
+    $this->strength = $strength;
   }
 
-  function getPv() {
-    return $this->pv;
+  function getPv() { return $this->pv; }
+  function setPv(int $pv) {
+    if($pv >= 0 && $pv <= $this->pvMax) {
+      $this->pv = $pv;
+    }
+  }
+
+  public function strike(Alive $otherAlive) {
+    $otherAlive->setPv($otherAlive->getPv() - $this->strength);
+}
+
+  public function __toString() {
+    $this->pv;
   }
 }
 
 $martin = new Alive();
+echo $martin->getPv(), '<br>';
+$martin->strike($martin);
 echo $martin->getPv();
