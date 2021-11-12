118a26eafb526c38c9a2096410f1dcdcc5f51e2c
MartinJeanne <martinjeanne.dev@gmail.com>
2021-11-12 09:37:25 +0100

    Merge fix

--- main.php
+++ main.php
@@ -1,16 +1,15 @@
 <?php
-function _require_all($dir) {
+function require_all($dir) {
     $scan = glob("$dir/*");
     foreach ($scan as $path) {
         if (preg_match('/\.php$/', $path)) {
             require_once $path;
         } elseif (is_dir($path)) {
-            _require_all($path);
+            require_all($path);
         }
     }
 }
-
-_require_all('models');
+require_all('models');
 
 function say($msg) {
     echo $msg . '<br>';
@@ -28,7 +27,6 @@ $dirt = new Dirt();
 $steve->breakBlock($dirt);
 
 $t = $steve->getInventory();
-
 foreach ($t as $v) {
     say($v);
 }
